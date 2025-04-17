5107eee966d45ec1b0d71de530167980b5fae2c0
Tannoura <koussay.zarrouk@esprit.tn>
2025-04-17 03:09:05 +0100

    upload

--- mission-service/src/main/java/com/example/missionservice/controller/missionController.java
+++ mission-service/src/main/java/com/example/missionservice/controller/missionController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@RequestMapping("/mission")
 public class missionController {
 
     private final missionService missionservice;
